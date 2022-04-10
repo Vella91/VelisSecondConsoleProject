6bfa2b90d538dd9a1bf49bdcd9f6199b1f029574
Vella91 <velichka.suvandzhieva@mentormate.com>
2022-04-10 19:47:25 +0300

    adding exercise5 - vacation logic

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,38 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("What do you prefer: Mountain or Beach? Enter 1 for Mountain and 2 for Beach.");
+        int vacationPreference = myScanner.nextInt();
+        System.out.println("You prefer for a vacation is: " + vacationPreference);
+
+
+        if (vacationPreference != 1 && vacationPreference != 2) {
+            System.out.println("There is no such vacation preference right now.");
+        } else {
+            if (vacationPreference == 1) {
+                System.out.println("You prefer the Mountain, so let's see how much is your budget per day per person.");
+            } else System.out.println("You prefer the Beach, so let's see how much is your budget per day per person.");
+
+            System.out.println("Please enter your allowed budget per day for one person for this vacation.");
+            double allowedBudgetPerDayPerPerson = myScanner.nextInt();
+
+            if (allowedBudgetPerDayPerPerson <= 0) {
+                System.out.println("This is not a valid budget for this vacation. Please enter budget that is larger than 0.");
+            }
+            if (vacationPreference == 1 && allowedBudgetPerDayPerPerson >= 50) {
+                System.out.println("I will advise that we look for your Mountain holiday somewhere outside of Bulgaria, for example Romania.");
+            } else if (vacationPreference == 1) {
+                System.out.println("I will advise that we look for your Mountain holiday somewhere nice in Bulgaria, for example the Pirin mountain.");
+            } else if (allowedBudgetPerDayPerPerson >= 50) {
+                System.out.println("I will advise that we look for your Beach Holiday somewhere abroad, for example Greece.");
+            } else
+                System.out.println("Let's look for some nice Beach destinations in Bulgaria, for example Sinemorets.");
+        }
     }
 }
