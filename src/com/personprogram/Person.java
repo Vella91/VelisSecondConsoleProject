302f861983de5ee2822af5be06490871ba00fd43
Vella91 <velichka.suvandzhieva@mentormate.com>
2022-05-08 22:07:07 +0300

    Person class homework - there is a bug somewhere

--- src/com/personprogram/Person.java
+++ src/com/personprogram/Person.java
@@ -65,9 +65,7 @@ public class Person {
 
     public boolean checkIfWrongSex(){
         try {
-            boolean isWrongSex = false;
             if (sex != "male" && sex != "female") {
-                isWrongSex = true;
                 System.out.println("The person's sex is a wrong value. Please provide a correct sex: male or female");
             }
             else System.out.println("The person's sex is: " + sex);
@@ -78,13 +76,37 @@ public class Person {
         return false;
     }
 
+    private String sayHello() {
+        String sayHelloInEnglish = null;
+        try {
+            String userLanguage = language;
+            sayHelloInEnglish = "Hello";
+            if (userLanguage == "Italian") {
+                System.out.println("Ciao!");
+            } else if (userLanguage == "Bulgarian") {
+                System.out.println("Zdr ko pr!");
+            } else if (userLanguage == "English")
+                System.out.println(sayHelloInEnglish);
+            else System.out.println("We don't support such a language option to say hello right now, buuuut...");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return sayHelloInEnglish;
+    }
+
     public static void main(String[] args) {
         Person person1 = new Person("Daniel", "male", "Christian", "Bulgarian", "Developer", "Bulgarian",  "Bulgaria");
         System.out.println(person1.checkIfWrongSex());
+        System.out.println(person1.sayHello());
         //TODO wrong sex implementation to be added
 
-        Person person2 = new Person("Pablo", "wrong", "Jewish", "Spanish", "Marketing expert", "Cuban", "Cuba");
+        Person person2 = new Person("Pablo", "wrong", "Judaism", "Spanish", "Marketing expert", "Cuban", "Cuba");
         System.out.println(person2.checkIfWrongSex());
+        System.out.println(person2.sayHello());
+
+        Person person3 = new Person("Margaret", "female", "Islam", "Italian", "Pilot", "English", "Australia");
+        System.out.println(person2.checkIfWrongSex());
+        System.out.println(person2.sayHello());
     }
 
 
